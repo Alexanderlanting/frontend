f6727ae0db79673a7a3c8cd414d42626fc49f4a0
Alexanderlanting <90254397+Alexanderlanting@users.noreply.github.com>
2021-09-22 18:19:19 +0200

    hovers, added hamburger menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,10 @@
 // JavaScript Document
+menu = document.querySelector ("header img:nth-of-type(2)"); 
+headerul = document.querySelector ("header ul")
+
+
+menu.addEventListener ("click", ToggleMenu);
+
+function ToggleMenu() {
+headerul.classList.toggle("togglemenu")
+}
