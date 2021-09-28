49eb47849e467e64d6a8b96377ceed1e48f76420
Alexanderlanting <90254397+Alexanderlanting@users.noreply.github.com>
2021-09-28 17:12:06 +0200

    Basic carousel functionality, added focus states

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,30 @@
 // JavaScript Document
-menu = document.querySelector ("header img:nth-of-type(2)"); 
-headerul = document.querySelector ("header ul")
+menu = document.querySelector("header img:nth-of-type(2)");
+headerul = document.querySelector("header ul")
 
 
-menu.addEventListener ("click", ToggleMenu);
+menu.addEventListener("click", ToggleMenu);
 
 function ToggleMenu() {
-headerul.classList.toggle("togglemenu")
+    headerul.classList.toggle("togglemenu")
+};
+
+/* Carousel javascript */
+
+carouselRight = document.querySelector("main > section:nth-of-type(1) > button:nth-of-type(2)");
+
+CarouselLeft = document.querySelector("main > section:nth-of-type(1) > button:nth-of-type(1)");
+
+carousel = document.querySelector("main > section:nth-of-type(1) section");
+
+carouselRight.addEventListener("click", right);
+CarouselLeft.addEventListener("click", left);
+
+
+function right() {
+    carousel.classList.add("moveright")
+}
+
+function left() {
+    carousel.classList.remove("moveright")
 }
